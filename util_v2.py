655742618b95641e282cf403703f0e8e182f872a
PabloOrtiz20 <ortizpablo011@gmail.com>
2023-11-21 17:22:48 -0600

    V2

--- util_v2.py
+++ util_v2.py
@@ -1,24 +1,10 @@
 import string
 import easyocr
+import cv2
+import re
 
 # Initialize the OCR reader
 reader = easyocr.Reader(['en'], gpu=False)
-
-# Mapping dictionaries for character conversion
-dict_char_to_int = {'O': '0',
-                    'I': '1',
-                    'J': '3',
-                    'A': '4',
-                    'G': '6',
-                    'S': '5'}
-
-dict_int_to_char = {'0': 'O',
-                    '1': 'I',
-                    '3': 'J',
-                    '4': 'A',
-                    '6': 'G',
-                    '5': 'S'}
-
 
 def write_csv(results, output_path):
     """
@@ -59,53 +45,6 @@ def write_csv(results, output_path):
         f.close()
 
 
-def license_complies_format(text):
-    """
-    Check if the license plate text complies with the required format.
-
-    Args:
-        text (str): License plate text.
-
-    Returns:
-        bool: True if the license plate complies with the format, False otherwise.
-    """
-    if len(text) != 7:
-        return False
-
-    if (text[0] in string.ascii_uppercase or text[0] in dict_int_to_char.keys()) and \
-       (text[1] in string.ascii_uppercase or text[1] in dict_int_to_char.keys()) and \
-       (text[2] in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'] or text[2] in dict_char_to_int.keys()) and \
-       (text[3] in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'] or text[3] in dict_char_to_int.keys()) and \
-       (text[4] in string.ascii_uppercase or text[4] in dict_int_to_char.keys()) and \
-       (text[5] in string.ascii_uppercase or text[5] in dict_int_to_char.keys()) and \
-       (text[6] in string.ascii_uppercase or text[6] in dict_int_to_char.keys()):
-        return True
-    else:
-        return False
-
-
-def format_license(text):
-    """
-    Format the license plate text by converting characters using the mapping dictionaries.
-
-    Args:
-        text (str): License plate text.
-
-    Returns:
-        str: Formatted license plate text.
-    """
-    license_plate_ = ''
-    mapping = {0: dict_int_to_char, 1: dict_int_to_char, 4: dict_int_to_char, 5: dict_int_to_char, 6: dict_int_to_char,
-               2: dict_char_to_int, 3: dict_char_to_int}
-    for j in [0, 1, 2, 3, 4, 5, 6]:
-        if text[j] in mapping[j].keys():
-            license_plate_ += mapping[j][text[j]]
-        else:
-            license_plate_ += text[j]
-
-    return license_plate_
-
-
 def read_license_plate(license_plate_crop):
     """
     Read the license plate text from the given cropped image.
@@ -116,23 +55,35 @@ def read_license_plate(license_plate_crop):
     Returns:
         tuple: Tuple containing the formatted license plate text and its confidence score.
     """
-
-    detections = reader.readtext(license_plate_crop, min_size=300, allowlist="ABCDEFGHJKLMNPRSTUVWXYZ0123456789-")
-
     
+    box_size = 100
+    
+    license_plate_resized = cv2.resize(license_plate_crop, (260, 130))
 
+    detections = reader.readtext(license_plate_resized, allowlist="ABCDEFGHJKLMNPRSTUVWXYZ0123456789-", min_size=box_size)
+
+    # print(detections)
+    
+    while box_size >= 10:
+        
+        if len(detections) == 0: 
+            box_size -= 10
+            detections = reader.readtext(license_plate_resized, allowlist="ABCDEFGHJKLMNPRSTUVWXYZ0123456789-",min_size=box_size)
+        
+        if len(detections) >= 1:
+            # print([txt[1] for txt in detections])
+            break
+        
     for detection in detections:
         bbox, text, score = detection
-
-        text = text.upper().replace(' ', '')
-
-        #if license_complies_format(text):
-        #    return format_license(text), score
         
-        return text,score
+        if ("-" in text) and (len(text)>=7) and (len(text)<=10) and (len(re.findall(r"[0-9]{4}[-][0-9]{4}", text)) == 0):
+            text = text.upper().replace(' ', '')
+            return text,score
+        else:
+            continue
         
     return None, None
-
 
 def get_car(license_plate, vehicle_track_ids, vehicle_list):
     """
