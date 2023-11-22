5fa3c4d8f9048eb2bc323d4a1bae7d1064f66574
PabloOrtiz20 <ortizpablo011@gmail.com>
2023-11-21 18:07:55 -0600

    cleaning

---
+++ data_clean.py
@@ -0,0 +1,38 @@
+import pandas as pd
+
+#Necesitamos tomar fecha actual
+data_raw = pd.read_csv("./raw_data_2023-11-15.csv",dtype={
+    'frame_nmr': 'string',
+    #'frame_nmr': 'int',
+    'car_id': 'int',
+    'vehicle_type': 'string',
+    'car_bbox': 'string',
+    'license_plate_bbox':'string',
+    'license_plate_bbox_score':'float64',
+    'license_number':'string',
+    'license_number_score':'float64'})
+
+data_raw['frame_nmr'] = pd.to_datetime(data_raw['frame_nmr'])
+    
+car_type = data_raw.groupby(["car_id"])[["vehicle_type","license_number"]].agg(lambda x: x.value_counts().idxmax())
+
+car_id_count = data_raw.groupby(["car_id"])[["car_id"]].agg(lambda x: x.count())
+car_id_count = car_id_count.add_suffix("_1")
+
+interim = pd.merge(car_type,car_id_count,how="left",left_index=True,right_index=True)
+
+arrival = data_raw.groupby(["car_id"])[["frame_nmr"]].agg(lambda x: x.min())
+interim_2 = pd.merge(interim,arrival,how="left",left_index=True,right_index=True)
+
+leave = data_raw.groupby(["car_id"])[["frame_nmr"]].agg(lambda x: x.max())
+interim_3 = pd.merge(interim_2,leave,how="left",left_index=True,right_index=True)
+
+
+df_sorted = interim_3.sort_values('car_id_1', ascending=False).drop_duplicates('license_number')
+df_sorted["time"] = (df_sorted["frame_nmr_y"] - df_sorted["frame_nmr_x"]) / 30
+
+df_sorted.reset_index(inplace=True)
+df_sorted.columns = [["car_id","vehicle_type","license_number","car_count","det_start","det_end","tot_time"]]
+
+df_sorted[["vehicle_type","license_number","det_start","det_end"]].to_csv("./clean_final.csv",index=False)
+df_sorted[["vehicle_type","license_number","det_start","det_end"]].to_json("./clean_final.json")
